f57cfc1a0f66584a6d64e14510018169ec8e23d0
kkamara <kamaracomputers@gmail.com>
2025-05-04 13:09:05 +0100

    Create a basic Express app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log("test");
+import express from 'express';
+import path from 'path';
+import indexRouter from './routes/index.js';
+const app = express();
+const PORT = 3000;
+// Set up views and view engine
+app.set('views', path.join(process.cwd(), 'src/views'));
+app.set('view engine', 'pug');
+// Define routes
+app.use('/', indexRouter);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
